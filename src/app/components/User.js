90b9fa7f78ed19ebc52255b29ceb824d01a2d837
harsh bhama <harshbhama96@gmail.com>
2019-04-08 16:47:54 +0530

    add lists and delete

---
+++ src/app/components/User.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export const User = (props) =>{
+  return(
+    <li>
+    <span>name: {props.children} age: {props.age} </span>
+    <button onClick = {props.delevent}>DELETE</button>
+    </li>
+  )
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,30 +1,91 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import { Parent } from "./components/parenttochild/Parent"
+import { User } from "./components/User"
 
+// import { Parent } from "./components/parenttochild/Parent"
+//
+// class App extends React.Component{
+//
+//   state = {
+//     title: "Old Title"
+//   }
+//
+//   onChange(newtitle){
+//     this.setState({
+//       title:newtitle
+//     })
+//   }
+//   render(){
+//     return(
+//       <div>
+//           <Parent
+//           changeworld = {this.onChange.bind(this,"Change world")} title = {this.state.title}
+//           sameworld = {this.onChange.bind(this, "Same world")} title = {this.state.title}
+//           />
+//
+//       </div>
+//     )
+//   }
+// }
 class App extends React.Component{
 
-  state = {
-    title: "Old Title"
-  }
+    state = {
+        users: [
+          {name: 'abc', age: 20},
+          {name: 'cde', age: 21},
+          {name: 'efg', age: 22}
+        ]
+    }
 
-  onChange(newtitle){
-    this.setState({
-      title:newtitle
-    })
-  }
+    onDelete(index, e){
+      const users = Object.assign([], this.state.users)
+      users.splice(index, 1)
+      this.setState({
+        users:users
+      })
+    }
   render(){
     return(
-      <div>
-          <Parent
-          changeworld = {this.onChange.bind(this,"Change world")} title = {this.state.title}
-          sameworld = {this.onChange.bind(this, "Same world")} title = {this.state.title}
-          />
 
-      </div>
+          <div>
+              <ul>
+                {
+                  this.state.users.map((user, index) => {
+                    return(<User age = {user.age}
+                      delevent = {this.onDelete.bind(this, index)}
+                      >{user.name}</User>)
+                  })
+                }
+              </ul>
+          </div>
+
     )
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"))
